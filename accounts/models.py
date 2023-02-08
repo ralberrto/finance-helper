dc5326fff488a916dece261753c00262fd21783e
Rafael Alberto Larios <albertolarios@me.com>
2023-02-07 21:36:05 -0600

    Add Accounts model to accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=35)
+    holder = models.CharField(max_length=50)
+    currency = models.CharField(max_length=20)
+    locked = models.BooleanField()
+    comment = models.CharField(max_length=50)
