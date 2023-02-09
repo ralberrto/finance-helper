a4674e8e354eec1173b4530a0ab9f1fb5260c3be
Rafael Alberto Larios <albertolarios@me.com>
2023-02-09 07:36:57 -0600

    Add url route to Accout retrieve-update api view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import AccountListAPIView, AccountDetailsAPIView, AccountCreateAPIView
+from . import views
 
 
 urlpatterns = [
-    path("accounts/", AccountListAPIView.as_view(), name="account_list"),
-    path("accounts/<int:id>/", AccountDetailsAPIView.as_view(), name="account_details"),
-    path("accounts/create/", AccountCreateAPIView.as_view(), name="account_create"),
+    path("accounts/", views.AccountListAPIView.as_view(), name="account_list"),
+    path(
+        "accounts/<int:id>/",
+        views.AccountDetailsAPIView.as_view(),
+        name="account_details",
+    ),
+    path(
+        "accounts/create/", views.AccountCreateAPIView.as_view(), name="account_create"
+    ),
+    path(
+        "accounts/update/<int:id>",
+        views.AccountRetrieveUpdateAPIView.as_view(),
+        name="account_update",
+    ),
 ]
