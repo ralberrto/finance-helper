835c3acdc415d57c10c1879f4e17eeaa3737876b
Rafael Alberto Larios <albertolarios@me.com>
2023-02-09 06:48:32 -0600

    Add url route to Account create api view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AccountListAPIView, AccountDetailsAPIView
+from .views import AccountListAPIView, AccountDetailsAPIView, AccountCreateAPIView
 
 
 urlpatterns = [
     path("accounts/", AccountListAPIView.as_view(), name="account_list"),
     path("accounts/<int:id>/", AccountDetailsAPIView.as_view(), name="account_details"),
+    path("accounts/create/", AccountCreateAPIView.as_view(), name="account_create"),
 ]
