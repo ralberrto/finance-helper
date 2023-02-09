d7dbf07575ede533c3b01086d6838e53cae0d0a2
Rafael Alberto Larios <albertolarios@me.com>
2023-02-09 05:53:59 -0600

    Add Account details api view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Account
-from .serializers import AccountListSerializer
+from .serializers import AccountListSerializer, AccountDetailsSerializer
 
 
 class AccountListAPIView(generics.ListAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountListSerializer
+
+
+class AccountDetailsAPIView(generics.RetrieveAPIView):
+    lookup_field = "id"
+    queryset = Account.objects.all()
+    serializer_class = AccountDetailsSerializer
