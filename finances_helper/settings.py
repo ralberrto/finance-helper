fb6c63eeada338dd383b9997e561e8071ded3e5b
Rafael Alberto Larios <albertolarios@me.com>
2023-02-12 15:25:49 -0600

    Fix mistype in settings

--- finances_helper/settings.py
+++ finances_helper/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIR = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
